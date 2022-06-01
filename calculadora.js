2dbc480b21743e1ebc9f5f133557f1a504b4a92c
Facuu18 <facuolave98@gmail.com>
2022-06-01 19:05:34 -0300

    agrego funcionalidades de calculadora básica (suma, resta, multiplicación y división)

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+require ('./sumaYResta');
+require ('./multiplicacionYDivision');
+
+console.log(suma(2,5));
+console.log(resta(-5,18));
+console.log(multiplicacion(75,15));
+console.log(division(11,10))
---
+++ multiplicacionYDivision.js
@@ -0,0 +1,3 @@
+module.exports= 
+multiplicacion= (a,b)=>a*b
+division= (a,b)=>a/b
